a1601d8526ccbaab1f8d8b4a5a324c53dc23276b
Piotr Wesolowski <kontakt@piotrwesolowski.pl>
2023-01-23 23:06:40 +0100

    Empty project has been created

---
+++ MVPExample/MVPExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVPExample
+//
+//  Created by Piotr Wesołowski on 23/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
